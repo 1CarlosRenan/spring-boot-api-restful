cf903249ec4a7e6442268bc5f4693f86cd7e41cb
CarÃlos Renan <carlosrenan96@gmail.com>
2024-04-30 23:30:20 -0300

    feat: new person service

--- src/main/java/com/carlosrenan/services/PersonServices.java
+++ src/main/java/com/carlosrenan/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.carlosrenan.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,15 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
 	public Person findById(String id) {
 		
 		logger.info("Finding one person!");
@@ -21,6 +32,16 @@ public class PersonServices {
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Carlos");
 		person.setLastName("Renan");
+		person.setAddress("brasil");
+		person.setGender("Male");
+		return person;
+	}
+	
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Carlos " + i);
+		person.setLastName("Renan " + i);
 		person.setAddress("brasil");
 		person.setGender("Male");
 		return person;
