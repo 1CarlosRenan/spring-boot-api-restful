3f348afe4d721c3e88a26df45e15a3e34abce61c
CarÃlos Renan <carlosrenan96@gmail.com>
2024-05-09 22:54:30 -0300

    feat: add hateoas and mockito

--- src/main/java/com/carlosrenan/repositories/PersonRepository.java
+++ src/main/java/com/carlosrenan/repositories/PersonRepository.java
@@ -1,10 +1,7 @@
 package com.carlosrenan.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.carlosrenan.model.Person;
 
-
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
